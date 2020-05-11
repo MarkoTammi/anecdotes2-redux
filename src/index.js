9f53c0a945a008476c6e17cc917c04adcc194abe
Tammivuori <marko.tammivuori@gmail.com>
2020-05-11 09:07:34 +0300

    6.9 anekdootit, step7

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
+
+
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+//import anecdoteReducer from './reducers/anecdoteReducer'
 
-const store = createStore(reducer)
+//const store = createStore(anecdoteReducer)
+
+import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
